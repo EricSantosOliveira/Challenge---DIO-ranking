f3631b57f687fbfaaf7fa37ac5b77b579c1fd394
Eric <ericmarcalmusic@outlook.com>
2024-02-28 22:46:01 -0300

    DesafioDioRankingVitoriasDerrotas

---
+++ DioChallengeRanking.js
@@ -0,0 +1,20 @@
+let resultado = soma (145,46)
+function soma (QtdVitorias, QtdDerrotas){
+let saldo = QtdVitorias - QtdDerrotas
+}
+
+if (saldo <10){
+    console.log("ferro")}
+  else if (saldo > 11 < 20) { 
+    console.log("Bronze")}
+  else if (saldo > 21 < 50) { 
+    console.log("prata")
+  }else if (saldo > 51 < 80) { 
+    console.log("Ouro")}
+  else if (saldo > 81 < 90) { 
+    console.log("Diamante")}
+  else if (saldo > 91 <100) { 
+    console.log("Lendário")}
+  else if (saldo > 101) { 
+    console.log("Imortal")}
+  
